9e800fe50dd82ab4137378e6d0b1b77dc7ae626f
MrRav3n <dawidmrraven@gmail.com>
2021-02-17 21:47:44 +0100

    website structure: connection to solidity smart contract (account address, account balance (KittyUniverse Tokens), one token price and add the functionality of purchasing tokens

---
+++ www/smart-contract-plugin.js
@@ -0,0 +1,358 @@
+﻿let web3;
+let contract;
+let tokenPrice;
+const onClickBuyTokens = () => {
+    document.querySelector("#buyTokensButton")
+        .addEventListener("click", e => buyTokens(e));
+}
+const buyTokens = async (e) => {
+    e.preventDefault();
+    let inputElement = document.getElementById("tokensAmount");
+    const tokensAmount = inputElement.value;
+    const tokensPrice = tokensAmount * tokenPrice;
+    await contract.methods.buyTokens(tokensAmount).send({from: web3.eth.defaultAccount, value: tokensPrice})
+    await setBalance("balance");
+    alert("Udało się!")
+    inputElement.value = "";
+}
+
+
+
+async function startApp() {
+    web3 = await getWeb3();
+    contract = await getContract();
+    await setDefaultAccount("account");
+    await setBalance("balance");
+    tokenPrice = await getOneTokenPrice();
+    setOneTokenPrice("tokenPrice"); 
+    console.log(web3.eth.defaultAccount);
+    onClickBuyTokens();
+}
+
+const getWeb3 = () => {
+    return new Promise((resolve, reject) => {
+        window.addEventListener("load", async () => {
+            if (window.ethereum) {
+                const web3 = new Web3(window.ethereum);
+                try {
+                    await window.ethereum.request({ method: "eth_requestAccounts" });
+                    resolve(web3);
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                reject("Must install MetaMask");
+            }
+        });
+    });
+};
+
+const getOneTokenPrice = async () => {
+    return await contract.methods.oneTokenPriceInWEI().call();
+};
+
+const setOneTokenPrice = (elementId) => {
+    
+    document.getElementById(elementId).innerText = web3.utils.fromWei(tokenPrice);
+}
+
+const setDefaultAccount = async (elementId) => {
+    const accounts = await web3.eth.getAccounts();
+    web3.eth.defaultAccount = accounts[0];
+    document.getElementById(elementId).innerText = accounts[0];
+};
+
+const setBalance = async (elementId) => {
+    const balance = await contract.methods.balanceOf(web3.eth.defaultAccount).call();
+    document.getElementById(elementId).innerText = balance;
+}
+
+const getContract = async () => {
+    const contractAbi = [
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "total",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "tokenOwner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "spender",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "tokens",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Approval",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "delegate",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "numTokens",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokensAmount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "buyTokens",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "receiver",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "numTokens",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transfer",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "tokens",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Transfer",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "buyer",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "numTokens",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transferFrom",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "delegate",
+                    "type": "address"
+                }
+            ],
+            "name": "allowance",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "tokenOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "decimals",
+            "outputs": [
+                {
+                    "internalType": "uint8",
+                    "name": "",
+                    "type": "uint8"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "ethPriceInUSD",
+            "outputs": [
+                {
+                    "internalType": "int256",
+                    "name": "",
+                    "type": "int256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "name",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "oneTokenPriceInWEI",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "symbol",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+    const contractAddress = "0xc4cE14D3605361e4A47521eA45346C99000543D4";
+    return new web3.eth.Contract(
+        contractAbi,
+        contractAddress
+    );
+};
+
+startApp();
